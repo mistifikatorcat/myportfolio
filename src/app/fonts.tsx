d9253877fd1184a0531a2df22195934a4481a5a6
mistifikatorcat <92595353+mistifikatorcat@users.noreply.github.com>
2023-06-17 17:01:48 +0300

    fonts attempt

---
+++ src/app/fonts.tsx
@@ -0,0 +1,13 @@
+import { Montserrat, Open_Sans } from 'next/font/google'
+ 
+export const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+  display: 'swap',
+})
+ 
+export const opensans = Open_Sans({
+  subsets: ['latin'],
+  variable: '--font-opensans',
+  display: 'swap',
+})
