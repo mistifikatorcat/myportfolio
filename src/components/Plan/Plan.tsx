02bf6ec4e5e340491c6c966f1d75a55e88a6c00a
mistifikatorcat <92595353+mistifikatorcat@users.noreply.github.com>
2024-04-21 12:31:24 +0300

    add new section, fix some issues

--- src/components/Plan/Plan.tsx
+++ src/components/Plan/Plan.tsx
@@ -1,34 +1,48 @@
-
-import styles from  './Plan.module.scss';
+import styles from "./Plan.module.scss";
 
 function Plan({
-    title,
-    steps,
-    haveTo,
-    align
-}:{
-    title: string;
-    steps: string[];
-    haveTo: string[];
-    align: string;
-})
-{
-    return(
-        <div className={`${styles.plan} ${align === 'left' ? styles.left : styles.right}`} data-aos={`${align === 'left' ? ('fade-left') : ('fade-right')}`}
-        >
-          <h4>{title}</h4>
-          <ul> What have I done:
-            {steps.map((step) => (
-                <li key={step}>{step}</li>
-            ))}
-          </ul>
-          <ul>What I have to do:
-          {haveTo.map((item) => (
-                <li key={item}>{item}</li>
-            ))}
-          </ul>
-        </div>
-    );
+  title,
+  steps,
+  haveTo,
+  align,
+}: {
+  title: string;
+  steps: string[];
+  haveTo: string[];
+  align: string;
+}) {
+  return (
+    <div
+      className={`${styles.plan} ${
+        align === "left" ? styles.left : styles.right
+      }`}
+      data-aos={`${align === "left" ? "fade-left" : "fade-right"}`}
+    >
+      <h4 className={styles.title}>{title}</h4>
+
+      <span className={styles.subtitle}>What have I done:</span>
+
+      <ul
+        className={`${styles.steps} ${
+          align === "left" ? styles.stepLeft : styles.stepRight
+        }`}
+      >
+        {steps.map((step) => (
+          <li key={step}>{step}</li>
+        ))}
+      </ul>
+      <span className={styles.subtitle}>What I have to do:</span>
+      <ul
+        className={`${styles.haveTo} ${
+          align === "left" ? styles.planLeft : styles.planRight
+        }`}
+      >
+        {haveTo.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Plan;
