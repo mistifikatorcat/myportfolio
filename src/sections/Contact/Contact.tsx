0dcadb9e80305b2a7ad515696575c3d059d93a58
Daniel Evgrafov <danyaevgrafov@gmail.com>
2025-04-18 14:28:23 +0300

    mobile version

--- src/sections/Contact/Contact.tsx
+++ src/sections/Contact/Contact.tsx
@@ -17,20 +17,22 @@ function Contact() {
                     <p className={styles.text}>I'm always open to new ideas, partnerships, or cool side projects. Let's talk! </p>
                     <p className={styles.text}>Or you can just mail me at: </p>
                     <Link href='mailto:danyaevgrafov@gmail.com' className={styles.link}>danyaevgrafov@gmail.com</Link>
-                    <p className={styles.text}>Also you can reach out to me (or keep up to date on what I do) right here:</p>
                     <div className={styles.social}>
-                        <Link href="https://github.com/mistifikatorcat" target="_blank" rel="noopener noreferrer">
-                            <Image src="/images/icons/github.svg" alt="GitHub" width={30} height={30} className={styles.icon}/>
-                        </Link>
-                        <Link href="https://linkedin.com/in/danyaevgrafov" target="_blank" rel="noopener noreferrer">
-                            <Image src="/images/icons/linkedin.svg" alt="LinkedIn" width={30} height={30} className={styles.icon}/>
-                        </Link>
-                        <Link href="https://t.me/mistycat" target="_blank" rel="noopener noreferrer">
-                            <Image src="/images/icons/telegram.svg" alt="Telegram" width={30} height={30} className={styles.icon}/>
-                        </Link>
-                        <Link href="https://discord.com/users/mistifikatorcat" target="_blank" rel="noopener noreferrer">
-                            <Image src="/images/icons/discord.svg" alt="Discord" width={30} height={30} className={styles.icon}/>
-                        </Link>
+                        <p className={styles.text}>Also you can reach out to me (or keep up to date on what I do) right here:</p>
+                        <div className={styles.icons}>
+                            <Link href="https://github.com/mistifikatorcat" target="_blank" rel="noopener noreferrer">
+                                <Image src="/images/icons/github.svg" alt="GitHub" width={30} height={30} className={styles.icon} />
+                            </Link>
+                            <Link href="https://linkedin.com/in/danyaevgrafov" target="_blank" rel="noopener noreferrer">
+                                <Image src="/images/icons/linkedin.svg" alt="LinkedIn" width={30} height={30} className={styles.icon} />
+                            </Link>
+                            <Link href="https://t.me/mistycat" target="_blank" rel="noopener noreferrer">
+                                <Image src="/images/icons/telegram.svg" alt="Telegram" width={30} height={30} className={styles.icon} />
+                            </Link>
+                            <Link href="https://discord.com/users/mistifikatorcat" target="_blank" rel="noopener noreferrer">
+                                <Image src="/images/icons/discord.svg" alt="Discord" width={30} height={30} className={styles.icon} />
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/sections/Plans/Plans.tsx
+++ src/sections/Plans/Plans.tsx
@@ -1,28 +1,28 @@
-import Plan from "@/components/Plan/Plan";
-import styles from "./Plans.module.scss";
-import { plans } from "@/utils/variables";
-// import Image from "next/image";
-// import Link from "next/link";
+// import Plan from "@/components/Plan/Plan";
+// import styles from "./Plans.module.scss";
+// import { plans } from "@/utils/variables";
+// // import Image from "next/image";
+// // import Link from "next/link";
 
-function Plans() {
-  return (
-    <section className={styles.plans}>
-      <h2>What am I currently up to: </h2>
-        <br />
-        <div className={styles.planBoard}>
-          {plans.map((plan, index) => (
-            <Plan
-            key={index}
-            title={plan.title}
-            steps={plan.steps}
-            haveTo={plan.haveTo}
-            align={index % 2 === 0 ? 'left' : 'right'}
-            />
-          ))}
-        </div>
+// function Plans() {
+//   return (
+//     <section className={styles.plans}>
+//       <h2>What am I currently up to: </h2>
+//         <br />
+//         <div className={styles.planBoard}>
+//           {plans.map((plan, index) => (
+//             <Plan
+//             key={index}
+//             title={plan.title}
+//             steps={plan.steps}
+//             haveTo={plan.haveTo}
+//             align={index % 2 === 0 ? 'left' : 'right'}
+//             />
+//           ))}
+//         </div>
         
-    </section>
-  );
-}
+//     </section>
+//   );
+// }
 
-export default Plans;
+// export default Plans;
